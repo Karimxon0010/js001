ddc6189815513435f99f5f77d99ad01d37d1f494
Karimxon0010 <nelchyut@gmail.com>
2024-07-01 06:40:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+alert("14 yoshli Bozorov Karimxon Samarqand shahrida yashaydi ") 
+
+
+
+
+alert(30 + "06" + 2024)
+
+console.log(30, "06", 2024)
+
+
+
+let num1 = 5
+ let num2 = 100
+ let num3 = 5
+ let num4 =20
+ let num5 = 25
+
+ console.log(num1 * num4,num3**4,num2 / num5)
+
+
+
+let letCons = alert("alert intruziv oynada xabarni ko'rsatadi va siz ushbu oynani yopguningizcha skript oqimining bajarilishini to'xtatadi. console.log skriptning bajarilishini to'xtatmasdan konsolda xabarni turini ham ko'rsatadi.")
+
+console.log(letCons)
+
+
+
+let num = 41
+let ft = false
+let text = "Salom"
+let tf = 2022
+
+console.log(num, ft, text, tf       )
+
+
+
+console.log(alert)
